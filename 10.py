2bdb2733f4034ecd10bd397ad540da4ec543005c
Nandhin98 <47237438+Nandhin98@users.noreply.github.com>
2019-03-27 16:19:00 +0530

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,23 @@
+def square():
+	                min=999
+	                  (r,l)=([],[])
+	                  	for j in range(2):
+		                  	l.append(int(input()))
+		                           r.append(l)
+		                for m in l:
+		                    	if min>m:
+		                    		min=m
+	               	l=[]
+             	f=1
+                	for i in range(len(r)):
+           		dif=r[i][0]-r[i][1]
+	        		continue
+	          		f=0
+	                   	break
+	                             if f!=0:
+	             	print('yes')
+	                else :
+                                  		print('no')
+                   try:
+                   	square()
+                     	print('invalid')                         
